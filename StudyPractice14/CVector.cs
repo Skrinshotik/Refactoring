95453e6b4320afb55e38302a3a218798224010e9
Skrinshotik <vany.pozitivnyj@mail.ru>
2023-03-09 16:54:05 +0300

    Initial commit
    This is my code that works, but without any refactoring.

---
+++ StudyPractice14/CVector.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP14
+{
+    public class CVector<T>
+    {
+        private T[] vector;
+        private int size;
+
+        public CVector(int size)
+        {
+            this.size = size;
+            vector = new T[size];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= size)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range");
+                }
+                return vector[index];
+            }
+            set
+            {
+                if (index < 0 || index >= size)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range");
+                }
+                vector[index] = value;
+            }
+        }
+
+        public static CVector<T> operator +(CVector<T> v1, int num)
+        {
+            CVector<T> result = new CVector<T>(v1.size);
+            for (int i = 0; i < v1.size; i++)
+            {
+                dynamic x = v1[i];
+                dynamic y = num;
+                result[i] = x + y;
+            }
+            return result;
+        }
+
+        public static CVector<T> operator -(CVector<T> v1, CVector<T> v2)
+        {
+            if (v1.size != v2.size)
+            {
+                throw new Exception("Vectors have different sizes");
+            }
+            CVector<T> result = new CVector<T>(v1.size);
+            for (int i = 0; i < v1.size; i++)
+            {
+                dynamic x = v1[i];
+                dynamic y = v2[i];
+                result[i] = x - y;
+            }
+            return result;
+        }
+
+        public static bool operator ==(CVector<T> v1, CVector<T> v2)
+        {
+            if (v1.size != v2.size)
+            {
+                return false;
+            }
+            for (int i = 0; i < v1.size; i++)
+            {
+                if (!v1[i].Equals(v2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool operator !=(CVector<T> v1, CVector<T> v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public delegate void VectorCheckedEventHandler(bool result);
+        public event VectorCheckedEventHandler VectorChecked;
+
+        public void CheckVector()
+        {
+            bool result = true;
+            for (int i = 1; i < size; i++)
+            {
+                if (!vector[i].Equals(vector[i - 1]))
+                {
+                    result = false;
+                    break;
+                }
+            }
+            if (VectorChecked != null)
+            {
+                VectorChecked(result);
+            }
+        }
+
+        ~CVector()
+        {
+            Console.WriteLine("CVector object destroyed");
+        }
+        public string ToString()
+        {
+            string result = "";
+            int count = 0;
+            foreach (var i in vector)
+            {
+                if (count != size)
+                    result = result + $"{i}, ";
+                else
+                    result = result + $"{i}";
+            }
+            return result;
+        }
+    }
+}
---
+++ StudyPractice14/CollectionType.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP14
+{
+    public class CollectionType<T>
+    {
+        public Dictionary<int, T>[] collections;
+
+        public CollectionType(Dictionary<int, T>[] collections)
+        {
+            this.collections = collections;
+        }
+
+        public Dictionary<int, T>[] FindCollectionsWithNegativeElements()
+        {
+            List<Dictionary<int, T>> positiveCollections = new List<Dictionary<int, T>>();
+            foreach (Dictionary<int, T> collection in this.collections)
+            {
+                foreach (T element in collection.Values)
+                {
+                    if (element is int && Convert.ToInt32(element) < 0)
+                    {
+                        positiveCollections.Add(collection);
+                        break;
+                    }
+                }
+            }
+            return positiveCollections.ToArray();
+        }
+
+        public Dictionary<int, T> FindMaxCollection()
+        {
+            Dictionary<int, T> maxCollection = this.collections[0];
+            foreach (Dictionary<int, T> collection in this.collections)
+            {
+                if (collection.Count > maxCollection.Count)
+                {
+                    maxCollection = collection;
+                }
+            }
+            return maxCollection;
+        }
+
+        public Dictionary<int, T> FindMinCollection()
+        {
+            Dictionary<int, T> minCollection = this.collections[0];
+            foreach (Dictionary<int, T> collection in this.collections)
+            {
+                if (collection.Count < minCollection.Count)
+                {
+                    minCollection = collection;
+                }
+            }
+            return minCollection;
+        }
+
+        public Dictionary<int, T> FindCollectionWithElement(T element)
+        {
+            foreach (Dictionary<int, T> collection in this.collections)
+            {
+                if (collection.ContainsValue(element))
+                {
+                    return collection;
+                }
+            }
+            return null;
+        }
+
+        public void SortCollections()
+        {
+            Array.Sort(this.collections, (x, y) => x.Count.CompareTo(y.Count));
+        }
+
+        public void Print()
+        {
+            foreach (var i in collections)
+            {
+                foreach (var j in i)
+                {
+                    Console.Write($"{j}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public static class DictionaryExtension
+    {
+        public static string ToString<T>(this Dictionary<int,T> dict)
+        {
+            string result = "";
+            foreach(var i in dict)
+            {
+                result += i.Value.ToString()+"  ";
+            }
+            return result;
+        }
+    }
+}
